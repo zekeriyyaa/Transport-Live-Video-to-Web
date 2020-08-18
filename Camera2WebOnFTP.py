943e36916a38f4d91250676210d90a8ee8ab25a2
Zekeriyya Demirci <54662519+zekeriyyaa@users.noreply.github.com>
2020-08-18 11:08:41 +0300

    Update Camera2WebOnFTP.py

--- Camera2WebOnFTP.py
+++ Camera2WebOnFTP.py
@@ -1,6 +1,7 @@
-"""Access IP Camera in Python OpenCV"""
 """
+Access IP Camera in Python OpenCV
 contact: zekeriyyademirci61@gmail.com
+
 """
 
 import cv2
@@ -11,37 +12,31 @@ from io import BytesIO
 
 # Use the next line if your camera has a username and password
 # stream = cv2.VideoCapture('protocol://username:password@IP:port/1')
-streamCam1 = cv2.VideoCapture("rtsp://admin:ifarlab1@192.168.8.118:554/cam/realmonitor?channel=1&subtype=0")
-streamCam2 = cv2.VideoCapture("rtsp://admin:ifarlab1@192.168.8.119:554/cam/realmonitor?channel=1&subtype=0")
+streamCam1 = cv2.VideoCapture("rtsp://username:passwd@192.168.10.118:554/cam/realmonitor?channel=1&subtype=0")
+
 fpsLimit = 0.5 # set time limit
+
 startTime = time.time()
+
 while True:
+    # start reading frame
     rCam1, frameCam1 = streamCam1.read()
-    rCam2, frameCam2 = streamCam2.read()
 
     nowTime = time.time()
     if (int(nowTime - startTime)) >= fpsLimit:
         print("current time: "+str(nowTime))
-        # do other cv2 stuff....
+        # for see the frame coming from IP Camera, can use imshow()
         #cv2.imshow('IP Camera stream', frame)
-        #dt=datetime.now()
-        #timeStr=str(dt.year)+"."+str(dt.month)+"."+str(dt.day)+"_"+str(dt.hour)+"."+str(dt.minute)+"."+str(dt.second)+"."+str(dt.microsecond)
+        # for the store frame w,th .jps ext on your local
         #cv2.imwrite('C:/Users/zekeriyya/Desktop/NodeVideo/test/cam' + timeStr + '.jpg', frame)
 
         ## write frames data to FTP server
-        ftp = FTP('ifarlab.evobulut.com', 'Ajk323NU77h2d', '$547Xmyn')
+        ftp = FTP('DOMAIN-NAME', 'USERNAME', 'PASSWD')
 
-        # store Cam1 frame on memory
+        # store Cam1 frame on memory not local
         retval, buffer = cv2.imencode('.jpg', frameCam1)
         file = BytesIO(buffer)
         ftp.storbinary('STOR /httpdocs/cam1/camera1.jpg', file)  # send the file
-        file.close()  # close file and FTP
-
-
-        # store Cam2 frame on memory
-        retval, buffer = cv2.imencode('.jpg', frameCam2)
-        file = BytesIO(buffer)
-        ftp.storbinary('STOR /httpdocs/cam2/camera2.jpg', file)  # send the file
         file.close()  # close file and FTP
 
         ftp.quit()
